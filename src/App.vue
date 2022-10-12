14eb1bb535d95b55225efc2b359cc54557f7d1dc
mather <misaka@misaka.im>
2022-10-12 16:16:05 +0800

    feat: first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <main>
+        <header>
+            <img src="./assets/pinia-logo.svg" alt="pinia logo">
+            <h1>Pinia Tasks</h1>
+        </header>
+
+        <div class="task-list">
+            <div v-for="task in taskStore.tasks">
+                <TaskDetails :task="task"/>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+    import TaskDetails from './components/TaskDetails.vue'
+    import { useTaskStore } from './stores/TaskStore';
+    export default {
+        components: { TaskDetails },
+        setup(){
+            const  taskStore = useTaskStore()
+            return { taskStore }
+        }
+    }
+</script>
---
+++ src/stores/TaskStore.js
@@ -0,0 +1,12 @@
+import { defineStore } from 'pinia'
+
+export const useTaskStore = defineStore('taskStore', {
+    state: () => ({
+        tasks: [
+            {id: 1, title: 'buy some milk', isFav: false},
+            {id: 2, title: 'play GloomHaven', isFav: false}
+        ],
+        name: 'Mather'
+    })
+
+})
